07bfa095c3bf89da1e637dedd2b7c4f52963d7c0
nelsondude <alexn1336@gmail.com>
2019-04-26 18:49:44 -0400

    finished about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Header from "./Header";
 
 export default (props)  => {
   return (
     <div>
-      <h1>About</h1>
+      <Header text={'About Us'}/>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec maximus accumsan ipsum, et iaculis nisi dignissim vitae. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut porttitor leo ligula, vel dignissim nisl mollis in. Sed elementum semper erat. In interdum, velit sit amet sodales congue, turpis diam efficitur justo, ut suscipit ex elit id ante. Aliquam vitae ultricies lacus. Suspendisse vulputate lectus a porta congue. Duis condimentum erat ac odio accumsan convallis. Morbi venenatis hendrerit odio in feugiat. Cras sed ultricies elit.
+        <br/>
+        <br/>
+        Sed sodales tristique imperdiet. Integer nec facilisis sem. Duis ut lobortis nulla. Etiam tincidunt facilisis pretium. Sed auctor magna vitae lacus congue, ut rutrum est pretium. Duis dignissim tincidunt massa, ut commodo elit dignissim dapibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Morbi tortor massa, pulvinar a libero vel, vehicula suscipit orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+      </p>
     </div>
   )
 }
