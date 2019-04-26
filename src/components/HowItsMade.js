f434ad734155ca504e41f516a09f413a49085ce5
Roman Kaufman <roman.a.kaufman@gmail.com>
2019-04-26 16:22:17 -0400

    pic + env

--- src/components/HowItsMade.js
+++ src/components/HowItsMade.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export default (props)  => {
   return (
-    <div>
-      <h1>How Its made</h1>
-    </div>
+    <Container>
+      <Row>
+        <h1>How Its made</h1>
+      </Row>
+
+    </Container>
   )
 }
